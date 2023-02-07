f8e15ad1d51ea2a999fd97b522db7c3ede4ae42f
The-Debarghya <debarghya4444@gmail.com>
2023-02-07 11:30:25 +0530

    fix

--- Assn2/automated_workflow.sh
+++ Assn2/automated_workflow.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 containerid=$(docker ps -aqf "name=file")
-docker container rm $containerid
-docker rmi file-store
+docker container rm $containerid 2>/dev/null
+docker rmi heisenberg8622/file-store
 docker compose up -d
-exit
